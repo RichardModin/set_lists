f49ab8e1ff7fc4906691b4c9d7df84252efb4bad
RichardModin <richard.modin@gmail.com>
2025-05-15 11:31:21 -0400

    feat: adding some print css for the setlist detail template

--- set_lists/settings.py
+++ set_lists/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'templates']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
